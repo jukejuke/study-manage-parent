3952a1024ff8d820f92a2f70e83a9edbc9c956a5
jk_hoap <7385697@qq.com>
2021-06-28 19:47:00 +0800

    first commit

---
+++ study-manage-api/src/main/java/com/juke/studymanageapi/StudyManageApiApplication.java
@@ -0,0 +1,13 @@
+package com.juke.studymanageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyManageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyManageApiApplication.class, args);
+	}
+
+}
